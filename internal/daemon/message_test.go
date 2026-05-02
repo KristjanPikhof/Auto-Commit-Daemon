bbbfe7d5cc80c3d5212cef9bd3f0b8b9584c1362
KristjanPikhof <kristjan.pikhof@gmail.com>
2026-05-02 16:54:47 +0300

    Add time import to message tests
    - Import time to support duration-based assertions in daemon message
      tests.

--- internal/daemon/message_test.go
+++ internal/daemon/message_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/KristjanPikhof/Auto-Commit-Daemon/internal/ai"
 	"github.com/KristjanPikhof/Auto-Commit-Daemon/internal/git"
